5c47a8aa437a5f48a7f884211b0d89441bfb4e70
Daniel Inoa <danielinoa@gmail.com>
2016-09-16 23:02:01 -0400

    Setups initial project

---
+++ Hoopy/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Hoopy
+//
+//  Created by Daniel Inoa on 9/16/16.
+//  Copyright © 2016 Daniel Inoa. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
